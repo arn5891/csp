c5ff540e90b0b82bc5789aa715adcd4810e2951a
edenpowers <eden.powers00@gmail.com>
2024-05-13 13:37:23 -0400

    created Team object, refactored end display

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#all code written by AN unless stated otherwise
+#all_teams code written by AN unless stated otherwise
 import math
 gens = ("II", "III", "IV")
 def get_average(lst, ind):
@@ -6,15 +6,15 @@ def get_average(lst, ind):
     for j in lst:
         found = False
         for i in counts:
-            if j[ind] == i[0] or (j[ind] not in gens and (i[0] in j[ind])):
-                i[1] = i[1]+1
+            if j[ind] == i.name or (j[ind] not in gens and (i.name in j[ind])):
+                i.members = i.members+1
                 found = True
         if found == False:
             counts.append([j[ind],1])
             
     max = ["¬", -100]
     for i in counts:
-        if i[1] > max[1]:
+        if i.members > max[1]:
             max = i
     return max
     
@@ -42,7 +42,7 @@ def check_input(string, spec):
     return a
 
 
-def Spartan():
+def make_spartan():
     name = check_input("\nInput name of Spartan: ", 1)
     tag = input("Input tag(numbers and letters only): ")
     generation = check_input("Input generation of Spartan(2-4): ", (0,(2,4)))
@@ -56,39 +56,42 @@ class Spartan():
         self.generation = generation
         self.color = color
         self.weapon = weapon
-    
+class Team():
+    def __init__(self, name, members) -> None:
+        self.name = name
+        self.members = members
 
-all = []
+all_teams = []
 print("Welcome to Halo Fireteam Generator.")
-#line 55 prints out ASCII art. Cut off in PDF, but it's nothing important to functionality.
+#next line prints out ASCII art. Cut off in PDF, but it's nothing important to functionality.
 print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠖⠋⠁⢀⡟⢿⠛⠛⠛⠋⠉⠉⠉⠛⢻⠛⡆⠀⠉⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢎⣥⡖⠀⢀⣀⣼⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢳⣀⡀⠀⠰⡦⡙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠟⠋⠉⠁⢀⡏⠳⣾⠀⠀⠀⠀⠀⠀⠤⠀⢾⠖⠙⡆⠈⠉⠉⠻⣿⣾⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡼⠼⠓⠒⠂⠀⠀⣿⣆⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⡆⣠⡟⠀⠀⠒⠒⠚⠧⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢠⠞⠉⠀⠀⠀⠀⠀⠀⠀⢸⠀⠹⡧⣤⣤⣤⣤⣤⣤⣤⣤⡤⡟⠁⣇⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢸⠀⣀⣤⣴⠶⠶⠛⠛⠛⠯⢽⡾⠿⠿⢿⣿⣿⣿⣿⣿⢿⣿⣷⠾⠿⠛⠛⠻⠶⠶⣦⣤⣀⠀⢳⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀ ⠀⠀⠀⣸⠾⠿⣥⣤⠤⠔⠒⠒⠂⠈⠉⠉⢉⠉⡀⠈⠉⠉⠉⠉⢉⣉⣉⠉⡀⠐⠒⠒⠲⠦⢤⣬⠿⠽⢾⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢰⡇⠀⡰⠋⣀⣤⠄⠒⠒⠊⢉⡩⠥⠀⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢍⡉⠁⠒⠒⠢⢤⣀⡉⢢⢀⣈⡇⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢹⠛⣷⣿⠁⠱⢄⠀⢀⠔⠉⠀⠀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠈⠲⢄⠀⣀⠼⠁⢹⣿⡟⣿⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢸⠀⣿⣧⠀⠀⠀⠈⠁⠀⠀⡠⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢦⡀⠀⠀⠉⠀⠀⠀⢸⣿⠀⣿⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢸⠀⣿⣿⠀⠀⠀⢀⡤⠒⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠒⠤⡀⠀⠀⠀⣌⣿⠀⣻⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣼⣀⣿⣯⣇⠀⠀⠘⣆⠀⠀it does stuff.  ⢀⠇⠀⠀⢠⣿⣿⣀⣸⡀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⣠⣾⣿⣯⣿⠻⡝⢦⡀⠀⠈⠑⢤⣀⣀⣀⡴⠀⠀⠀⠀⠀⠀⢢⣀⣀⣀⣠⠔⠁⠀⠀⣰⢿⠟⢻⣩⣿⣿⡄⠀⠀⠀⠀⠀\n⣖⣶⣾⣿⣿⣿⣿⣿⡟⣧⠀⠈⠲⣝⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⢞⡷⠁⠀⢸⡿⣾⣿⣭⣿⣿⣷⣶⣶\n⡟⣉⣉⡁⢸⣿⣿⣿⣹⡿⣄⠀⠀⠈⠢⣉⠲⢄⡀⠀⢀⠔⢋⣉⣉⣉⣉⣉⠲⣄⠀⢀⣠⠖⢋⠴⠋⠀⠀⢠⡿⣿⣽⣿⣿⣿⣋⣉⣙⣿\n⣩⣿⣿⣿⣿⣛⣿⢿⣦⣀⣿⡧⣄⠀⠀⠈⠙⠒⠬⣙⣳⣶⣟⣾⣷⣲⣿⣿⣷⣞⣋⠩⠔⠊⠁⠀⠀⢀⠴⣿⣄⣠⡿⢿⣟⣿⣿⣿⣿⣽\n⡿⠏⠁⠀⠀⠀⠀⠀⣘⣿⣿⣿⣮⡳⡄⠀⠀⠀⠀⠀⠈⠙⣿⠉⠉⠉⠉⢹⡏⠁⠀⠀⠀⠀⠀⢀⢔⣡⣾⣿⣿⣏⠀⠀⠀⠀⠀⠀⠉⠿\n⣁⣀⣠⣤⣴⣾⣿⡉⠀⣿⠙⢿⣿⣽⣟⠄⠀⠀⠀⠀⣠⣴⣿⣄⣀⣀⣀⣸⣧⣄⠀⠀⠀⠀⠀⢿⣿⣿⣿⠟⡿⠈⠉⣷⣶⣦⣤⣄⣀⣀\n⢉⣿⣿⣟⣿⣿⣿⣷⡀⠘⡄⠀⢻⡿⣟⠀⠀⠀⣠⠞⠁⡏⠉⠉⠉⠉⠉⠉⢹⠇⠑⢄⠀⠀⠀⣨⣿⡟⠁⠀⡗⢀⣴⣿⣿⣿⣿⣿⣾⣿\n⡸⢿⣿⣿⣀⣤⣿⣟⣻⠄⠃⠀⢸⡇⠉⠻⢿⣏⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⣩⣴⠟⠋⠀⡇⠀⣸⠁⢾⡛⠻⠥⣀⣽⣿⡿⢻\n⠑⣤⣉⠤⠜⠛⠉⠁⠀⠀⠘⡄⢸⡇⠀⣤⠾⠋⠑⢄⠀⢣⠀⠀⠀⠀⠀⠀⢸⠀⣠⠞⠙⠻⣦⡀⢨⡇⢠⠃⠀⠀⠀⠉⠒⠢⠤⣉⣧⣾\n⣼⣿⣤⣤⣤⣤⣤⣤⣤⣤⣤⣼⣿⡧⣤⡟⠀⠀⠀⠀⠑⣾⡷⠶⠶⠶⠶⣶⣿⡋⠁⠀⠀⠀⢹⣇⡼⣿⠯⠤⠤⠤⣤⣤⣤⢤⣤⣬⣭⣿\n⠈⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠳⣄⣿⡐⣦⣤⣀⣼⡟⠀⠀⠀⠀⠀⠀⠹⣷⢀⣠⣴⢲⣿⣯⠖⠁⠀⠀⠀⠀⣸⣟⣿⠀⣟⢛⣃⢹")
 print("Input the roster for a team of Spartan super-soldiers, including") 
 print("their: \n- name\n- tag(numbers and letters)\n- generation(2-4)\n- armor color\n- weaponry")
 num_teams = check_input("How many teams would you like to input? ", (0, (-math.inf, math.inf)))
 for i in range(num_teams):
-    team = [[],[]]
-    team[0] = check_input("Name of Fireteam: ", 1)
-    for i in range(check_input(f"How many Spartans are in {team[0]} Team? ",(0, (-math.inf, math.inf)))):
-        team[1].append(Spartan())
-    all.append(team)
+    team_name = check_input("Name of Fireteam: ", 1)
+    members = []
+    for i in range(check_input(f"How many Spartans are in {team_name} Team? ",(0, (-math.inf, math.inf)))):
+        members.append(make_spartan())
+    all_teams.append(Team(team_name, members))
 
-for i in all:
+for i in all_teams:
     
-    print(f"\nRoster for {i[0]} Team:")
-    for j in i[1]:
-        print(f"{j[0]}-{j[1]} is a Spartan-{j[2]}. Their armor color is {j[3]} and they carry a {j[4]}.")
+    print(f"\nRoster for {i.name} Team:")
+    for member in i.members:
+        print(f"{member.name}-{member.tag} is a Spartan-{member.generation}. Their armor color is {member.color} and they carry a {member.weapon}.")
 
-    if get_average(i[1], 2)[1] == 1:
-        print(f"No spartans in {i[0]} Team share generations.")
+    if get_average(i.members, 2)[1] == 1:
+        print(f"No spartans in {i.name} Team share generations.")
     else:
-        print(f"\nMost common generation for spartans in {i[0]} Team is {get_average(i[1], 2)[0]}\n{get_average(i[1], 2)[1]} Spartans have it.")
+        print(f"\nMost common generation for spartans in {i.name} Team is {get_average(i.members, 2)[0]}\n{get_average(i.members, 2)[1]} Spartans have it.")
     
-    if get_average(i[1], 3)[1] == 1:
-        print(f"No spartans in {i[0]} Team share armor colors.")
+    if get_average(i.members, 3)[1] == 1:
+        print(f"No spartans in {i.name} Team share armor colors.")
     else:
-        print(f"\nMost common armor color for {i[0]} Team is {get_average(i[1], 3)[0]}\n{get_average(i[1], 3)[1]} Spartans have it.")
+        print(f"\nMost common armor color for {i.name} Team is {get_average(i.members, 3)[0]}\n{get_average(i.members, 3)[1]} Spartans have it.")
     
-    if get_average(i[1], 4)[1] == 1:
-        print(f"No spartans in {i[0]} Team share common weapons.")
+    if get_average(i.members, 4)[1] == 1:
+        print(f"No spartans in {i.name} Team share common weapons.")
     else:
-        print(f"\nMost common weapon of choice in {i[0]} Team is {get_average(i[1], 4)[0]}\n{get_average(i[1], 4)[1]} Spartans have it.")
+        print(f"\nMost common weapon of choice in {i.name} Team is {get_average(i.members, 4)[0]}\n{get_average(i.members, 4)[1]} Spartans have it.")
